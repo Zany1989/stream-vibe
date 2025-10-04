776b7d67f0ef166a6b4b481d520e0c19b3cf9385
Zane Zeltina <zanezeltina32@inbox.lv>
2025-10-04 16:57:59 +0300

    Start to create a page footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,57 @@
+import "./Footer.scss"
+
 const Footer = () => {
-  return <footer>FOOTER</footer>
+  
+  const menuItems = [
+    {
+      title: "Home",
+      links: ["Categories", "Devices", "ricing", "FAQ"]
+    },
+    {
+      title: "Movies",
+      links: ["Genres", "Trending", "New Release", "Popular"]
+    },
+    {
+      title: "Shows",
+      links: ["Genres", "Trending", "New Release", "Popular"]
+    },
+    {
+      title: "Support",
+      links: ["Contact Us"]
+    },
+    {
+      title: "Subscription",
+      links: ["Plans", "Features", "ricing", "FAQ"]
+    },
+    {
+      title: "Connect With Us",
+      socialLinks: [
+        {
+          label: "Facebook",
+          iconName: "facebook",
+        },
+        {
+          label: "Twitter",
+          iconName: "twitter",
+        },
+        {
+          label: "LinkedIn",
+          iconName: "linkedin",
+        }
+      ]
+    }
+  ]
+  
+  return (
+    <footer className="footer">
+      <div className="footer__inner container">
+        <nav className="footer__menu">
+          <div className="footer__menu-column"></div>
+        </nav>
+        <div className="footer__extra"></div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
