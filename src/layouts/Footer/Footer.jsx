3a9f224698f65ebb5959167e4b8013f28baaab4c
Zane Zeltina <zanezeltina32@inbox.lv>
2025-10-06 20:50:35 +0300

    Finish page header and footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import "./Footer.scss"
+import Socials from "@/components/Socials";
 
 const Footer = () => {
   
@@ -36,19 +37,66 @@ const Footer = () => {
         },
         {
           label: "LinkedIn",
-          iconName: "linkedin",
+          iconName: "linked-in",
         }
       ]
     }
   ]
   
+  const extraLinks = ["Terms of Use", "Privacy", "Cookie Policy"]
+  
   return (
     <footer className="footer">
       <div className="footer__inner container">
         <nav className="footer__menu">
-          <div className="footer__menu-column"></div>
+          { menuItems.map(({title, links, socialLinks }, index) => (
+            <div className="footer__menu-column" key={index}>
+              <a
+                className="footer__menu-title h6"
+                href="/"
+              >
+                {title}
+              </a>
+              {links?.length > 0 && (
+                <ul className="footer__menu-list">
+                  {links.map((link, index) => (
+                    <li className="footer__menu-item" key={index}>
+                      <a
+                        className="footer__menu-link"
+                        href="/"
+                      >
+                        {link}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {socialLinks?.length > 0 && (
+                <Socials
+                  className="footer__soc1als"
+                  links={socialLinks}
+                />
+              )}
+            </div>
+          ))}
         </nav>
-        <div className="footer__extra"></div>
+        <div className="footer__extra">
+          <p className="footer__copyright">
+            @<time dateTime="2023">2023</time>
+            streamvib, All Rights Reserved
+          </p>
+          <div className="footer__extra-links">
+            {extraLinks.map((link, index) => (
+              <a
+                className="footer__extra-link"
+                href="/"
+                key={index}
+              >
+                {link}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   )
